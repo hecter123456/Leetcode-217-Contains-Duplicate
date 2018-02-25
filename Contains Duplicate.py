ef3198b488f789331b4374a60cb9db568462b82b
hecter <hecter654321@gmail.com>
2018-02-25 16:12:38 +0800

    pass testcase: testSample and testFalseSample by using dict

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -5,11 +5,27 @@ class unitest(unittest.TestCase):
         Input = []
         Output = False
         self.assertEqual(Solution().containsDuplicate(Input),Output)
+    def testSample(self):
+        Input = [1,2,3,1]
+        Output = True
+        self.assertEqual(Solution().containsDuplicate(Input),Output)
+    def testFalseSample(self):
+        Input = [1,2,3]
+        Output = False
+        self.assertEqual(Solution().containsDuplicate(Input),Output)
 
 class Solution():
     def containsDuplicate(self, nums):
-        if nums == []:
-            return False
+        dic = {}
+        for i in nums:
+            if i in dic:
+                dic[i] = -1
+            else:
+                dic[i] = 1
+        for key,value in dic.items():
+            if dic[key] == -1:
+                return True
+        return False
 
 if __name__ == '__main__':
     unittest.main()
